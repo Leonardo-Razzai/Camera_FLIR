3d40c17930df56ee7bbf30934b1de3dcd7ebfad2
LeonardoRazzai <xcoderx00@gmail.com>
2024-12-16 13:17:12 +0100

    Refactoring and good documentation

--- Trigger_image.py
+++ Trigger_image.py
@@ -1,10 +1,18 @@
 import PySpin
 import sys
 
-def init_camera()->tuple:
-    '''
-    Initialize FLIR camera
-    '''
+def init_camera() -> tuple:
+    """
+    Initialize the FLIR camera system and retrieve the first available camera.
+
+    Returns
+    -------
+    tuple
+        A tuple containing:
+        - cam (PySpin.CameraPtr): The initialized camera object.
+        - cam_list (PySpin.CameraList): List of connected cameras.
+        - system (PySpin.SystemPtr): PySpin system instance.
+    """
     cam = None
     print('*** INITIALIZING CAMERA ***\n')
     try:
@@ -16,190 +24,194 @@ def init_camera()->tuple:
         print('Error: %s' % ex)
     
     return cam, cam_list, system
-        
-def configure_trigger(cam)->bool:
-    """
-    This function configures the camera to use a trigger. First, trigger mode is
-    ensured to be off in order to select the trigger source. Trigger mode is
-    then enabled, which has the camera capture only a single image upon the
-    execution of the chosen trigger.
 
-     :param cam: Camera to configure trigger for.
-     :type cam: CameraPtr
-     :return: True if successful, False otherwise.
-     :rtype: bool
+def configure_trigger(cam) -> bool:
     """
+    Configure the camera to use a hardware or software trigger.
 
+    Ensures the trigger mode is off before setting the trigger source and activation type. 
+    Finally, enables the trigger mode for the camera to capture a single image upon trigger.
+
+    Parameters
+    ----------
+    cam : PySpin.CameraPtr
+        The camera to configure the trigger for.
+
+    Returns
+    -------
+    bool
+        True if the configuration is successful, False otherwise.
+    """
     print('*** CONFIGURING TRIGGER ***\n')
-
     try:
         result = True
 
-        # Ensure trigger mode off
-        # The trigger must be disabled in order to configure whether the source
-        # is software or hardware.
         if cam.TriggerMode.GetAccessMode() != PySpin.RW:
             print('Unable to disable trigger mode (node retrieval). Aborting...')
             return False
-
+        
         cam.TriggerMode.SetValue(PySpin.TriggerMode_Off)
-
         print('Trigger mode disabled...')
 
-        # Set TriggerSelector to FrameStart
-        # For this example, the trigger selector should be set to frame start.
-        # This is the default for most cameras.
-        if cam.TriggerSelector.GetAccessMode() != PySpin.RW:
-            print('Unable to get trigger selector (node retrieval). Aborting...')
-            return False
-
         cam.TriggerSource.SetValue(PySpin.TriggerSelector_FrameStart)
-
         print('Trigger selector set to frame start...')
 
-        # Select trigger source
-        # The trigger source must be set to hardware or software while trigger
-        # mode is off.
-        if cam.TriggerSource.GetAccessMode() != PySpin.RW:
-            print('Unable to get trigger source (node retrieval). Aborting...')
-            return False
-
         cam.TriggerSource.SetValue(PySpin.TriggerSource_Line0)
-        print('Trigger source set to hardware...')
+        print('Trigger source set to Line 0...')
 
-        # Turn trigger mode on
-        # Once the appropriate trigger source has been set, turn trigger mode
-        # on in order to retrieve images using the trigger.
-        cam.TriggerSelector.SetValue(PySpin.TriggerSelector_FrameStart)
         cam.TriggerActivation.SetValue(PySpin.TriggerActivation_RisingEdge)
+        print('Trigger activation set to RisingEdge...')
+        
         cam.TriggerMode.SetValue(PySpin.TriggerMode_On)
         print('Trigger mode turned back on...')
-
+        
     except PySpin.SpinnakerException as ex:
         print('Error: %s' % ex)
         return False
 
     return result
 
-
-def acquire_image(cam)->bool:
+def Start_acquisition(cam) -> bool:
     """
-    This function acquires and saves 10 images from a device.
-    Please see Acquisition example for more in-depth comments on acquiring images.
+    Start image acquisition on the camera in continuous mode.
 
-    :param cam: Camera to acquire images from.
-    :type cam: CameraPtr
-    :return: True if successful, False otherwise.
-    :rtype: bool
+    Sets the camera acquisition mode to continuous and begins acquisition. 
+    This function assumes an external trigger will control the capture timing.
+
+    Parameters
+    ----------
+    cam : PySpin.CameraPtr
+        The camera to acquire images from.
+
+    Returns
+    -------
+    bool
+        True if the acquisition is started successfully, False otherwise.
     """
     print('*** IMAGE ACQUISITION ***\n')
-    
-    image_result = None
-    
     try:
-        # Set acquisition mode to continuous
         if cam.AcquisitionMode.GetAccessMode() != PySpin.RW:
             print('Unable to set acquisition mode to continuous. Aborting...')
             return False
-
+        
         cam.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous)
         print('Acquisition mode set to continuous...')
 
-        #  Begin acquiring images
         cam.BeginAcquisition()
         print('Acquiring images: waiting for external trigger')
-
-        # Get device serial number for filename
-        device_serial_number = ''
-        if cam.TLDevice.DeviceSerialNumber.GetAccessMode() == PySpin.RO:
-            device_serial_number = cam.TLDevice.DeviceSerialNumber.GetValue()
-
-            print('Device serial number retrieved as %s...' % device_serial_number)
-
-        #  Retrieve next received image
-        image_result = cam.GetNextImage()
-
-        # End acquisition
-        cam.EndAcquisition()
-
+        
     except PySpin.SpinnakerException as ex:
         print('Error: %s' % ex)
-
-    #  Ensure image completion
-    if image_result.IsIncomplete():
-        print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
-        image_result = None
-    else:
-        print('Image acquired correcly')
+        return False
     
+    return True
+
+def GetImage(cam):
+    """
+    Retrieve the next image from the camera.
+
+    Blocks until an image is received or the timeout expires.
+
+    Parameters
+    ----------
+    cam : PySpin.CameraPtr
+        The camera to retrieve an image from.
+
+    Returns
+    -------
+    PySpin.ImagePtr or None
+        The retrieved image object, or None if an error occurs.
+    """
+    image_result = None
+    try:
+        image_result = cam.GetNextImage(500)
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
     return image_result
 
 def save_image(image_result, filename='Triggered_image.jpg'):
-    # Create ImageProcessor instance for post processing images
+    """
+    Save a captured image to a file after converting its format.
+
+    The image is converted to mono8 format and saved with the specified filename. 
+    Basic image information such as width and height is also printed.
+
+    Parameters
+    ----------
+    image_result : PySpin.ImagePtr
+        The captured image to process and save.
+    filename : str, optional
+        The filename to save the image as. Default is 'Triggered_image.jpg'.
+    """
     print('*** SAVING IMAGE ***\n')
-    
     try:
         processor = PySpin.ImageProcessor()
-
-        # Set image processor color processing method
         processor.SetColorProcessing(PySpin.SPINNAKER_COLOR_PROCESSING_ALGORITHM_HQ_LINEAR)
         
-        
-        #  Print image information
         width = image_result.GetWidth()
         height = image_result.GetHeight()
-        
         print('Processing image, width = %d, height = %d' % (width, height))
 
-        #  Convert image to mono 8
         image_converted = processor.Convert(image_result, PySpin.PixelFormat_Mono8)
-        # Save image
         image_converted.Save(filename)
-
         print('Image saved at %s\n' % filename)
 
-        #  Release image
         image_result.Release()
-        
+        print('Got image. Acquisition ended.')
     except PySpin.SpinnakerException as ex:
         print('Error: %s' % ex)
 
-def reset_trigger(cam)->bool:
+def reset_trigger(cam) -> bool:
     """
-    This function returns the camera to a normal state by turning off trigger mode.
+    Reset the camera by disabling the trigger mode.
 
-    :param cam: Camera to acquire images from.
-    :type cam: CameraPtr
-    :returns: True if successful, False otherwise.
-    :rtype: bool
+    Parameters
+    ----------
+    cam : PySpin.CameraPtr
+        The camera to reset the trigger mode for.
+
+    Returns
+    -------
+    bool
+        True if the trigger is successfully disabled, False otherwise.
     """
     try:
         result = True
-        # Ensure trigger mode off
-        # The trigger must be disabled in order to configure whether the source
-        # is software or hardware.
+
         if cam.TriggerMode.GetAccessMode() != PySpin.RW:
             print('Unable to disable trigger mode (node retrieval). Aborting...')
             return False
-
+        
         cam.TriggerMode.SetValue(PySpin.TriggerMode_Off)
-
         print('Trigger mode disabled...')
-
+        
     except PySpin.SpinnakerException as ex:
         print('Error: %s' % ex)
         result = False
 
     return result
-    
-def close_camera(cam, cam_list:list, system: PySpin.SystemPtr):
-    '''
-    End acquisition and deinitialize FLIR camera
-    '''
+
+def close_camera(cam, cam_list: list, system: PySpin.SystemPtr):
+    """
+    End image acquisition, deinitialize the camera, and release system resources.
+
+    Cleans up by stopping acquisition, deinitializing the camera, clearing the camera list, 
+    and releasing the PySpin system instance.
+
+    Parameters
+    ----------
+    cam : PySpin.CameraPtr
+        The camera to close.
+    cam_list : list
+        The list of connected cameras.
+    system : PySpin.SystemPtr
+        The PySpin system instance to release.
+    """
     try:
+        cam.EndAcquisition()
         cam.DeInit()
         cam_list.Clear()
         system.ReleaseInstance()
-        print('Camera correcly closed.')
+        print('Camera correctly closed.')
     except PySpin.SpinnakerException as ex:
         print('Error: %s' % ex)
